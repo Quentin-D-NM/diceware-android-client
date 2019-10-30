87c48cfaadf83fb45370c0bd19006b83ab9b7968
Quentin-D-NM <qdye@cnm.edu>
2019-10-30 16:54:32 -0600

    Refactor MainActivity add Refresh.

--- app/src/main/java/edu/cnm/deepdive/diceware/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/diceware/controller/MainActivity.java
@@ -3,26 +3,74 @@ package edu.cnm.deepdive.diceware.controller;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.RecyclerView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import edu.cnm.deepdive.diceware.R;
-import edu.cnm.deepdive.diceware.service.DicewareService;
 import edu.cnm.deepdive.diceware.service.GoogleSignInService;
 import edu.cnm.deepdive.diceware.view.PassphraseAdapteer;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.schedulers.Schedulers;
+import edu.cnm.deepdive.diceware.viewmodel.MainViewModel;
 
 public class MainActivity extends AppCompatActivity {
+
+  private ProgressBar waiting;
+  private RecyclerView passphraseList;
+  private MainViewModel viewModel;
+  private GoogleSignInService signInService;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    setupUI();
+    setupViewModel();
+    setupSignIn();
+  }
+
+  private void setupViewModel() {
+    viewModel = ViewModelProviders.of(this).get(MainViewModel.class);//get view model of this context for the MainViewModel instance
+    viewModel.getPassphrases().observe(this, (passphrases) -> {
+      PassphraseAdapteer adapteer = new PassphraseAdapteer(this, passphrases,
+          (view, position, passphrase) -> {
+            // TODO Add code to pop up editor.
+            Log.d("Passphrase click", passphrase.getKey());
+          },
+          (menu, position, passphrase) -> {
+            Log.d("Passphrase context", passphrase.getKey());
+            getMenuInflater().inflate(R.menu.passphrase_context, menu);
+            menu.findItem(R.id.delete_passphrase).setOnMenuItemClickListener(
+                (item) -> {
+                  Log.d("Delete selected", passphrase.getKey());
+                  waiting.setVisibility(View.VISIBLE);
+                  refreshSignIn(() -> viewModel.deletePassphrase(passphrase));
+                  return true;
+                });
+          });
+      passphraseList.setAdapter(adapteer);
+      waiting.setVisibility(View.GONE);
+    });
+    viewModel.getThrowable().observe(this, (throwable) -> {
+      if(throwable != null) {
+        waiting.setVisibility(View.GONE);
+        Toast.makeText(this, String.format("Unable to connect to server:%s", throwable.getMessage()), Toast.LENGTH_LONG).show();
+      }
+    });
+  }
+
+  private void setupSignIn() {
+    signInService = GoogleSignInService.getInstance();
+    signInService.getAccount().observe(this, (account) ->
+        viewModel.setAccount(account));
+  }
+
+  private void setupUI() {
     setContentView(R.layout.activity_main);
     Toolbar toolbar = findViewById(R.id.toolbar);
     setSupportActionBar(toolbar);
@@ -35,33 +83,8 @@ public class MainActivity extends AppCompatActivity {
             .setAction("Action", null).show();
       }
     });
-    RecyclerView passphraseList = findViewById(R.id.keyword_list);
-    GoogleSignInService.getInstance().getAccount().observe(this, (account) -> {
-      if (account != null) {
-        String token = getString(R.string.oauth_header, account.getIdToken());
-        Log.d("Oauth2.0 token", token); //FIXME Remove before shipping.
-        DicewareService.getInstance().getAll(token)
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe((passphrases) -> {
-              PassphraseAdapteer adapteer = new PassphraseAdapteer(this, passphrases,
-                  (view, position, passphrase) -> {
-                    //TODO Add code to pop up editor.
-                    Log.d("Passphrase click", passphrase.getKey());
-                  },
-                  (menu, position, passphrase) -> {
-                    Log.d("Passphrase context", passphrase.getKey());
-                    getMenuInflater().inflate(R.menu.passphrase_context, menu);
-                    menu.findItem(R.id.delete_passphrase).setOnMenuItemClickListener((item) -> {
-                      Log.d("Delete Selected", passphrase.getKey());
-                      //TODO Send request to server to delete passphrase; refresh view.
-                      return true;
-                    });
-                  });
-              passphraseList.setAdapter(adapteer);
-            });
-      }
-    });
+    waiting = findViewById(R.id.waiting);
+    passphraseList = findViewById(R.id.keyword_list);
   }
 
   @Override
@@ -74,18 +97,31 @@ public class MainActivity extends AppCompatActivity {
   public boolean onOptionsItemSelected(MenuItem item) {
     boolean handled = true;
     switch (item.getItemId()) {
+      case R.id.refresh:
+        refreshSignIn(() -> viewModel.refreshPassphrases());
+        break;
       case R.id.action_settings:
         break;
       case R.id.sign_out:
-        GoogleSignInService.getInstance().signOut().addOnCompleteListener((task) ->{
-          Intent intent = new Intent(this, LoginActivity.class);
-          intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-          startActivity(intent);
-        });
+        signOut();
         break;
       default:
         handled = super.onOptionsItemSelected(item);
     }
     return handled;
+  }
+
+  private void signOut() {
+    signInService.signOut().addOnCompleteListener((task) ->{
+      Intent intent = new Intent(this, LoginActivity.class);
+      intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+      startActivity(intent);
+    });
+  }
+
+  private void refreshSignIn(Runnable runnable) {
+    signInService.refresh()
+        .addOnSuccessListener((account) -> runnable.run())
+        .addOnFailureListener((e) -> signOut());
   }
 }
